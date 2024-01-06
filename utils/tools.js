fcf131e4a2fa7ec72ea22b86768a7d270a9a2fc0
Bilal Amjad <bilalarain444@gmail.com>
2024-01-07 01:03:14 +0500

    add docker support, update env, add composer

--- utils/tools.js
+++ utils/tools.js
@@ -1,10 +1,12 @@
 const jwt = require("jsonwebtoken");
 const { User } = require("./user");
 const { Math } = require("./math");
+const { Fancy } = require("./fancy");
 
 const Tools = {
   User,
   Math,
+  Fancy,
 };
 
 module.exports = { Tools };
