9b490c2456d47eb712382ab620ef25aba8d1a2a2
Bilal Amjad <bilalarain444@gmail.com>
2024-01-06 22:57:01 +0500

    add password hashing, add comparision

--- models/User.model.js
+++ models/User.model.js
@@ -2,6 +2,7 @@
 
 const { DatabaseConnection, Mongoose } = require("../db.config");
 const { Schema, model } = Mongoose;
+const bcrypt = require("bcrypt");
 
 const userSchema = new Schema({
   name: String,
@@ -31,9 +32,23 @@ const userSchema = new Schema({
 
 // set timestamp
 userSchema.pre("save", function (next) {
+  next();
+});
+// comparePassword
+userSchema.methods.comparePassword = async function (password) {
+  const isMatch = await bcrypt.compare(password, this.password);
+  return isMatch;
+};
+
+// hash password
+userSchema.pre("save", async function (next) {
   const currentDate = new Date();
   this.updated_at = currentDate;
   if (!this.created_at) this.created_at = currentDate;
+  if (!this.isModified("password")) return next();
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(this.password, salt);
+  this.password = hash;
   next();
 });
 
