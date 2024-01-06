5480c0851873dfae6d4c04e27a66fddaeea1c34b
Bilal Amjad <bilalarain444@gmail.com>
2024-01-06 22:56:21 +0500

    fix object refrence issues

--- middleware/checkin-logger.js
+++ middleware/checkin-logger.js
@@ -1,19 +1,12 @@
 async function checkinLogger(req, res, next) {
-  // ip
   const ip = req.headers["x-forwarded-for"] || req?.connection?.remoteAddress;
-  // method
   const method = req.method;
-  // path
   const path = req.path;
-  // query
-  const query = req.query;
-  // body
-  const body = req.body || "forbidden";
-  // params
-  const params = req.params;
-  // timestamp -dd/mm/yyyy hh:mm:ss
+  const query = { ...req?.query };
+  const body = { ...req?.body } || "forbidden";
+  if (body && body.password) delete body.password;
+  const params = { ...req?.params };
   const timestamp = new Date().toLocaleString();
-  // log
   console.log(
     `[${timestamp}] ${ip} ${method} ${path} ${JSON.stringify({
       query,
@@ -21,7 +14,6 @@ async function checkinLogger(req, res, next) {
       params,
     })}`
   );
-
   next();
 }
 
