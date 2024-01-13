35acc34268fa1c0993dda1c07af6e808cccbb5f6
Bilal Amjad <bilalarain444@gmail.com>
2024-01-13 20:40:53 +0500

    add cookie parser

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -5,11 +5,30 @@ const { UserController } = require("../controller/User.controller");
 
 const auth = async (req, res, next) => {
   try {
-    const token = req.header("Authorization").split(" ")[1] || req?.cookies?.token;
+    const authHeader = req.header("Authorization");
+    const cookieHeader = req?.cookies?.token;
+    let token = false;
+
+    if (authHeader) {
+      token = authHeader.replace("Bearer ", "");
+    } else if (cookieHeader) {
+      token = cookieHeader;
+    }
+    console.log("\n-----------------------------------\n");
+    // req.header
+    console.log("** AUTH MIDDLEWARE ** -> req.header:", req.header("Authorization"));
+    // authHeader: Bearer <token>
+    console.log("** AUTH MIDDLEWARE ** -> authHeader:", authHeader);
+    console.log("** AUTH MIDDLEWARE ** -> cookieHeader:", cookieHeader);
+    console.log("** AUTH MIDDLEWARE ** -> token:", token);
+    console.log("\n-----------------------------------\n");
+
+    if (!token) throw new Error("No token provided");
+
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     const userId = decoded._id;
     const user = await UserController.verifyUser(userId);
-    if (!user) throw new Error();
+    if (!user) throw new Error("No user found");
     req.user = user;
     req.token = token;
     console.log("\n-----------------------------------\n");
@@ -17,6 +36,9 @@ const auth = async (req, res, next) => {
     console.log("\n-----------------------------------\n");
     next();
   } catch (err) {
+    console.log("\n-----------------------------------\n");
+    console.log("** AUTH MIDDLEWARE ** -> ERROR:", err.message);
+    console.log("\n-----------------------------------\n");
     res.status(401).json({ message: "Please authenticate" });
   }
 };
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
 const { Tools } = require("./utils/tools");
 const { DatabaseConnection } = require("./db.config");
 const { checkinLogger, errorHandler, notFound } = require("./middleware");
@@ -18,6 +19,7 @@ app.use(
     credentials: true,
   })
 );
+app.use(cookieParser());
 // content parsing
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
